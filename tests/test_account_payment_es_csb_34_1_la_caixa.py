e75bb17de80882f247311db5acd4ce17ad4b0e33
resteve <resteve@zikzakmedia.com>
2015-12-01 11:00:40 +0100

    Tests

--- tests/test_account_payment_es_csb_34_1_la_caixa.py
+++ tests/test_account_payment_es_csb_34_1_la_caixa.py
@@ -1,30 +1,18 @@
-#!/usr/bin/env python
 # This file is part of the account_payment_es_csb_34_1_la_caixa module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountPaymentEsCSB341LaCaixaTestCase(unittest.TestCase):
-    '''Test Account Payment ES CSB 34-1 La Caixa module'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_payment_es_csb_34_1_la_caixa')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_payment_es_csb_34_1_la_caixa')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountPaymentEsCsb341LaCaixaTestCase(ModuleTestCase):
+    'Test Account Payment Es Csb 34 1 La Caixa module'
+    module = 'account_payment_es_csb_34_1_la_caixa'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountPaymentEsCSB341LaCaixaTestCase))
+        AccountPaymentEsCsb341LaCaixaTestCase))
     return suite
